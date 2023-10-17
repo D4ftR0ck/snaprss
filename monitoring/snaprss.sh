1d7937f2226a165c41db791cbc7ca74064471516
D4ftR0ck <86687768+D4ftR0ck@users.noreply.github.com>
2023-10-17 18:54:25 +0200

    Update snaprss.sh

--- monitoring/snaprss.sh
+++ monitoring/snaprss.sh
@@ -5,7 +5,7 @@ var_location=$(awk 'NR % 3 == 1' $current_dir/location)
 IFS=$'\n'
 lines=($var_location)
 
-#FOLDER CREATION
+#FOLDER CREATION WITH LOCATION FILE
 for line in "${lines[@]}"; do
     if [ ! -d $current_dir/$line ]; then
     mkdir -p $line
@@ -37,41 +37,61 @@ cp "$current_dir/location" "$current_dir/tempo2"
 while [ "$(wc -l < $current_dir/tempo1)" -gt 0 ];do
 tempo=($(head -n 3 $current_dir/tempo1))
 declare -a tempo
+# {tempo[0]} : name {tempo[1]} : gps {tempo[2} distance
 
-snapmap-archiver -o $current_dir/${tempo[0]}/ -l="${tempo[1]}" -r ${tempo[2]} --write-json
+lat=$(echo ${tempo[1]} | sed 's/ //g' | cut -d ',' -f1)
+lon=$(echo ${tempo[1]} | sed 's/ //g' | cut -d ',' -f2)
+
+epoch=$(curl -s -X POST "https://ms.sc-jpl.com/web/getLatestTileSet" -H "Content-Type: application/json" -d '{}' | jq -r .tileSetInfos[1].id.epoch)
+# or grep -oE '{"type":"HEAT","flavor":"default","epoch":"[0-9]+"\}' | grep -o "[0-9]*")
+
+curl -s -X POST "https://ms.sc-jpl.com/web/getPlaylist" -H "Content-Type: application/json" -d '{"requestGeoPoint": {"lat": '$lat', "lon": '$lon'}, "zoomLevel": 5, "tileSetId": {"flavor": "default", "epoch": "'$epoch'", "type": 1}, "radiusMeters": '${tempo[2]}', "maximumFuzzRadius": 0}' | jq '[.manifest.elements[] | {create_time: .timestamp, snap_id: .id, duration: .duration, file_type: .snapInfo.snapMediaType, url: .snapInfo.streamingMediaInfo.mediaUrl, mignature: .snapInfo.streamingThumbnailInfo.infos[].thumbnailUrl }]' > $current_dir/${tempo[0]}/archive.json
+sed -i -e 's/SNAP_MEDIA_TYPE_VIDEO_NO_SOUND/mp4/g' -e 's/SNAP_MEDIA_TYPE_VIDEO/mp4/g' -e 's/null/"jpg"/g' $current_dir/${tempo[0]}/archive.json
+
+jq -r '.[] | .create_time, .snap_id, .file_type, .url' $current_dir/${tempo[0]}/archive.json > $current_dir/${tempo[0]}/archive.json.tempo
+
     if [ $(wc -l < $current_dir/${tempo[0]}/archive.json) -lt 2 ]; then
-    echo "Le fichier archive.json n'a pas de nouveaux snaps"
-        if [ -e $current_dir/${tempo[0]}/archive.json.tempo ]; then rm $current_dir/${tempo[0]}/archive.json.tempo; fi
+    echo "Le fichier archive.json de ${tempo[0]} n'a pas de nouveaux snaps"
+    # si moins de 2 lignes pas de nouvelles données, suppression de archive.json.tempo.
+    rm $current_dir/${tempo[0]}/archive.json.tempo
     else
     jq -r '.[] | .create_time, .snap_id, .file_type, .url' $current_dir/${tempo[0]}/archive.json > $current_dir/${tempo[0]}/archive.json.tempo
     fi
+
+sleep $((5 + RANDOM % 10))
 sed -i '1,3d' $current_dir/tempo1
 done
 rm $current_dir/tempo1
 
-#monitoring
+#Traitement monitoring
 while [ "$(wc -l < $current_dir/tempo2)" -gt 0 ];do
 tempo=($(head -n 3 $current_dir/tempo2))
 declare -a tempo
-echo "$tempo est en cours de traitement"
 
-# si le fichier archive.json.tempo existe le script continue
 if [ ! -f $current_dir/${tempo[0]}/archive.json.tempo ];then
 sed -i '1,3d' $current_dir/tempo2
 
 else
+    echo "$tempo est en cours de traitement"
     while [ "$(wc -l < $current_dir/${tempo[0]}/archive.json.tempo)" -gt 0 ];do
-            json=($(head -n 4 $current_dir/${tempo[0]}/archive.json.tempo))
+            json=($(head -n 4 $current_dir/${tempo[0]}/archive.json.tempo)) # a modifier
             declare -a json
             datesnap=$(echo ${json[0]} | cut -c 1-10)
             datexml=$(date -Ru)
-            realdate=$(date -d @$datesnap) # add 'u' for universal TIME UTC
+            realdate=$(date -d @$datesnap) # ajouter 'u' pour la date universel UTC
             shorturl=$(echo ${json[3]} | rev | cut -c13- | rev)
-
-                if grep -q "${json[1]}" $current_dir/monitoring.xml;then
+            #creation du tableau
+                if grep -q "${json[1]}" $current_dir/monitoring.xml;then #verification que le fichier existe pas déjà
                 sed -i '1,4d' $current_dir/${tempo[0]}/archive.json.tempo
                 else
-                curl -s "https://story.snapchat.com/o/${json[1]}" > $current_dir/${tempo[0]}/urltempo
+
+                sleep $((3 + RANDOM % 5))
+
+                curl -s -o $current_dir/${tempo[0]}/${json[1]}.${json[2]} "${json[3]}"
+
+                sleep $((3 + RANDOM % 7))
+
+                curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" "https://www.snapchat.com/spotlight/${json[1]}" > $current_dir/${tempo[0]}/urltempo  #https://story.snapchat.com/o/
                 titre=$(grep -o '"title":"[^"]*"' $current_dir/${tempo[0]}/urltempo | head -n 2 | tail -n 1 | sed 's/"//g' | cut -d ':' -f 2)
                 titre2=$(grep -o '"title":"[^"]*"' $current_dir/${tempo[0]}/urltempo | head -n 1 | sed 's/"//g' | cut -d ':' -f 2)
 
@@ -89,53 +109,57 @@ else
                     echo -e '<description><![CDATA[<iframe width="1080" height="1920" src="\c' >> $current_dir/monitoring.xml
                     fi
                     echo -e "http://127.0.0.1:8556/${tempo[0]}/${json[1]}.${json[2]}\c" >> $current_dir/monitoring.xml
-#                   echo -e "http://localhost/snaprss/${tempo[0]}/${json[1]}.${json[2]}\c" >> $current_dir/monitoring.xml
+#                    echo -e "http://localhost/snaprss/${tempo[0]}/${json[1]}.${json[2]}\c" >> $current_dir/monitoring.xml
                     echo '"/><br >' >> $current_dir/monitoring.xml
 
-                if echo "$titre" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$titre" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "Titre1 :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "Titre1 : $titre<br >" >> $current_dir/monitoring.xml
                 fi
 
-                if echo "$titre2" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$titre2" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "Titre2 :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "Titre2 : $titre2<br >" >> $current_dir/monitoring.xml
                 fi
 
-                if echo "$pseudo" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$pseudo" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "Pseudo1 :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "Pseudo1 : $pseudo<br >" >> $current_dir/monitoring.xml
                 fi
 
-                if echo "$pseudo2" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$pseudo2" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "Pseudo2 :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "Pseudo2 : $pseudo2<br >" >> $current_dir/monitoring.xml
                 fi
 
-                if echo "$pageTitle" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$pageTitle" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "PageTitle :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "PageTitle : $pageTitle<br >" >> $current_dir/monitoring.xml
                 fi
 
-                if echo "$datareacthelmet" | grep -Eq "'s Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
+                if echo "$datareacthelmet" | grep -Eq "'s Sound|Spotlight Sound|Our Story on Snapchat|OVF Editor|Created for Spotlight|Tap to try it out\!|Spotlight on Snapchat|Create My Bitmoji|Your identity on Snapchat"; then
                     echo "data-react-helmet :<br >" >> $current_dir/monitoring.xml
                 else
                     echo "data-react-helmet : $datareacthelmet<br >" >> $current_dir/monitoring.xml
                 fi
 
                 myliste=("$titre" "$titre2" "$pseudo" "$pseudo2" "$pageTitle" "$datareacthelmet")
+                last=""
                     for var in "${myliste[@]}"; do
+                    if [[ "$var" != "$last" ]]; then
                     if [[ ! $var =~ [[:space:]] && $var =~ ([a-z]|-_\.|[0-9]) && ! $var =~ [A-Z] ]]; then
-                        echo "Probable Owner: https://www.snapchat.com/add/$var<br >" >> $current_dir/monitoring.xml
+                    echo "Probable Owner: https://www.snapchat.com/add/$var<br >" >> $current_dir/monitoring.xml
+                    last="$var"
+                            fi
                         fi
                     done
 
-                echo "Source: https://story.snapchat.com/o/${json[1]}<br >" >> $current_dir/monitoring.xml
+                echo "Source: https://www.snapchat.com/spotlight/${json[1]}<br >" >> $current_dir/monitoring.xml
                 echo "Date du snap : $realdate" >> $current_dir/monitoring.xml
 
                 echo -e "]]>\c" >> $current_dir/monitoring.xml
@@ -148,7 +172,6 @@ else
 sed -i '1,3d' $current_dir/tempo2
 rm $current_dir/${tempo[0]}/archive.json.tempo
 fi
-
 done
 rm $current_dir/tempo2
 
@@ -156,5 +179,5 @@ if ! grep -q "</rss>" $current_dir/monitoring.xml;then
 echo "</channel>" >> $current_dir/monitoring.xml
 echo "</rss>" >> $current_dir/monitoring.xml
 fi
-timeout 60 python3 -m http.server 8556 --bind 127.0.0.1 -d $current_dir/
-pkill -9 -f 'python3 -m http.server'
+#timeout 60 python3 -m http.server 8556 --bind 127.0.0.1 -d $current_dir/
+#pkill -9 -f 'python3 -m http.server'
